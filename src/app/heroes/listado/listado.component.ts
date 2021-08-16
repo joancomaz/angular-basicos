b568edfb7293084cbed4e8f9067e85124bbd92ef
joancomaz <joancordoba@gmail.com>
2021-08-16 23:28:38 +0200

    Primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent {
+
+  heroeBorrado: string = '';
+  heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor', 'Capitán América'];
+
+  borrarHeroe(): void {
+    console.log('Borrando...');
+    // this.heroes.splice(this.heroes.length-1, 1);
+    this.heroeBorrado = this.heroes.shift() || '';
+    // console.log(this.heroeBorrado);
+    
+  }
+
+}
